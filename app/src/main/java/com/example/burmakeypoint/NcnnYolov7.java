828baab946617ca89f31121a76233f49294f7d02
tondt <tondt@dyno.vn>
2024-01-03 08:59:07 +0700

    add

---
+++ app/src/main/java/com/example/burmakeypoint/NcnnYolov7.java
@@ -0,0 +1,18 @@
+package com.example.burmakeypoint;
+
+
+import android.content.res.AssetManager;
+import android.view.Surface;
+
+public class NcnnYolov7 {
+    public native boolean loadModel(AssetManager mgr, int modelid, int cpugpu);
+    public native boolean openCamera(int facing);
+    public native boolean closeCamera();
+    public native boolean setOutputWindow(Surface surface);
+
+    static {
+        System.loadLibrary("ncnnyolov7");
+    }
+
+
+}
